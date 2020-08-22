802464081a20a603ab90968602f8c00b22b44df3
Katarina <nymphadora.96@gmail.com>
2020-08-23 01:42:36 +0200

    removes items from cart when the amount is 0

--- web-store-client/src/app/cart-list/cart-list.component.ts
+++ web-store-client/src/app/cart-list/cart-list.component.ts
@@ -24,12 +24,16 @@ export class CartListComponent implements OnInit {
   }
 
   onMinus(item: ShoppingCartItem){
-    if(item.amount>0){
+    if(item.amount>1){
       item.amount=item.amount-1;
     }
     else{
-      
-    }
+
+      const index = this.items.indexOf(item, 0);
+      if (index > -1) {
+         this.items.splice(index, 1);
+        }
+      }
   }
   onPlus(item: ShoppingCartItem){
     item.amount=item.amount+1;
