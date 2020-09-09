60ef648215ccabed8a1d4531c4dfdfaf72cfe3d3
mi15104 <34017705+mi15104@users.noreply.github.com>
2020-09-09 19:22:38 +0200

    Visual studio bug

--- web-store-server/components/users/usersAPI.js
+++ web-store-server/components/users/usersAPI.js
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 
 userRouter.post("/login", async (req, res) => {
   const user = req.body;
-  //console.log(user.email);
+  
   var query  = userModel.where({ email: user.email });
   query.findOne(function (err, userInMongo) {
     console.log(user.email);
@@ -20,28 +20,17 @@ userRouter.post("/login", async (req, res) => {
     }
   
   })
-  // const userInMongo =  userModel.findOne({ email: user.email })
-  // console.log(user.email);
-  // console.log(userInMongo);
-  // if (
-  //   user.email === userInMongo.email &&
-  //   user.password === userInMongo.password
-  // ) {
-  //   return res.status(200).json({ message: "Login success!"});
-  // } else {
-  //   return res.status(403).json({ message: "Wrong email/password" });
-  // }
 });
 
 userRouter.post("/register", async (req, res) => {
   const user = req.body;
   // check if user email exists
   const query  = await userModel.findOne({ email: user.email }).exec();
-  console.log(query);
+  
   
   if(query==null){
     const userInMongo = new userModel(user);
-    console.log(userInMongo);
+    
     await userInMongo.save();
 
     return res.status(202).send();
