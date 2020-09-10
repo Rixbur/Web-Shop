bda45a158c025d839c9d452751af7c185be766e4
mi15104 <34017705+mi15104@users.noreply.github.com>
2020-09-10 22:03:28 +0200

    Added subscriptions for login and register
    In order to avoid memory leaks i added subscription arrays into login and register component

--- web-store-client/src/app/user/login/login.component.ts
+++ web-store-client/src/app/user/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { Subscribable, Subscription } from 'rxjs';
 // import { nameValidator } from './name-validator';
 
 @Component({
@@ -9,17 +10,23 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,OnDestroy {
   email: string = '';
   password: string = '';
-
+  activeSubscriptions: Subscription[] = []
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {}
+
+  ngOnDestroy(){
+    this.activeSubscriptions.forEach(sub=>sub.unsubscribe);
+  }
+
   public submitForm(): void {
     const subscribe = this.userService.login({
       email: this.email,
       password: this.password,
     });
+    this.activeSubscriptions.push(subscribe);
   }
 }
--- web-store-client/src/app/user/register/register.component.ts
+++ web-store-client/src/app/user/register/register.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit,OnDestroy {
   email: string = '';
   password: string = '';
-
+  private activeSubscriptions: Subscription[] = []
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {}
+  ngOnDestroy(){
+    this.activeSubscriptions.forEach(sub=>sub.unsubscribe);
+  }
   public submitForm(): void {
-    this.userService.register({
+    const subscription = this.userService.register({
       email: this.email,
       password: this.password,
     });
+    this.activeSubscriptions.push(subscription);
   }
 }
